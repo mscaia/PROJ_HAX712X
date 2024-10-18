db9b0c40f8d9facb2d4ea2e1ff2ab1050954f4b7
Kateryna Stetsun <kateryna.stetsun@etu.umontpellier.fr>
2024-10-18 08:49:31 +0200

    Remove unnecessary files and directories from roadmap branch

--- visualisation/visu.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import json
-import requests
-from fonctions_basedonnees import json_process
-
-url = "https://data.montpellier3m.fr/sites/default/files/ressources/MMM_EcoCompt_X2H19070220_archive.json"
-data = json_process(url,"../data/EcoCompt1.json")
