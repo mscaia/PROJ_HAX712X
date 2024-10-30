3375e4c78533b05f1936a008d349a5bd1757625c
Mattéo <matteo.s@outlook.fr>
2024-10-30 12:33:23 +0100

    ajout d'une stat

--- src/donnée.py
+++ src/donnée.py
@@ -44,32 +44,3 @@ url = "https://data.montpellier3m.fr/sites/default/files/ressources/MMM_EcoCompt
 path_target = "../data/EcoCompt1.json"
 path, fname = os.path.split(path_target)
 pooch.retrieve(url, path=path, fname=fname, known_hash=None)
-
-
-
-#Pour traiter les données sur la date
-# Convertir la colonne 'Departure' en datetime
-df_coursesvelomagg_traité['Departure'] = pd.to_datetime(df_coursesvelomagg_traité['Departure'])
-# Extraire les dates de départ
-df_coursesvelomagg_traité['Date'] = df_coursesvelomagg_traité['Departure'].dt.date
-
-# Compter le nombre de trajets par jour pour les départs
-trajets_depart = df_coursesvelomagg_traité.groupby('Date').size().reset_index(name='Nombre de trajets')
-
-# Remplir les NaN par 0 pour les dates sans trajets
-trajets_depart.fillna(0, inplace=True)
-
-# Afficher le résultat
-print("Nombre de trajets par jour:")
-print(trajets_depart)
-
-# Visualisation sous forme de diagramme à barres
-plt.figure(figsize=(12, 6))
-plt.bar(trajets_depart['Date'], trajets_depart['Nombre de trajets'], color='b', alpha=0.7)
-plt.title('Nombre total de trajets par jour (départs)')
-plt.xlabel('Date')
-plt.ylabel('Nombre total de trajets')
-plt.xticks(rotation=45)  # Rotation des étiquettes sur l'axe des x pour une meilleure lisibilité
-plt.grid(axis='y')  # Ajouter une grille horizontale pour une meilleure lisibilité
-plt.tight_layout()  # Pour ajuster les marges
-plt.show()
