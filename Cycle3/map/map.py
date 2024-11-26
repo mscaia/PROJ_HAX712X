a3267b2e5dfcbcd1fec649f9c420ec37d80a04d8
Kateryna Stetsun <kateryna.stetsun@etu.umontpellier.fr>
2024-11-26 01:13:41 +0100

    upd to last version (#44)
    * json
    
    * orga doss
    
    * ajout class
    
    * ajout class+modif
    
    * ajout class + utilisation
    
    * ajout carte interac + api + map
    
    * upd docum page + render
    
    ---------
    
    Co-authored-by: CharlotteARMAND <armand.charlotte34@gmail.com>
    Co-authored-by: Mattéo <matteo.s@outlook.fr>

---
+++ Cycle3/map/map.py
@@ -0,0 +1,74 @@
+import osmnx as ox
+import folium
+import networkx as nx
+
+# Ville ciblée pour extraire les données du réseau de routes cyclables
+ville = "Montpellier, France"
+G = ox.graph_from_place(ville, network_type="all")
+
+# Import des noms de station de vélo
+stations = ox.geometries_from_place(ville, tags={'amenity': 'bicycle_rental'})
+stations = stations['name'].dropna()
+Liste_des_stations = stations.tolist()
+print(Liste_des_stations)
+
+def choisir_station(prompt):
+    while True:
+        choix = input(prompt)
+        if choix in Liste_des_stations:
+            return choix
+        else:
+            print(f"'{choix}' n'est pas dans la liste des stations. Veuillez choisir parmi : {Liste_des_stations}")
+
+# Utilisation de ox.geocode() pour obtenir les coordonnées de l'origine et de la destination
+départ = choisir_station("Entrez le lieu de départ : ")
+arrivé = choisir_station("Entrez le lieu d'arrivée : ")
+
+# Ajout de ", Montpellier, France" pour s'assurer du géocodage correct
+origin = ox.geocode(f"{départ}, Montpellier, France")
+destination = ox.geocode(f"{arrivé}, Montpellier, France")
+
+# Trouver les nœuds les plus proches de l'origine et de la destination
+origin_node = ox.nearest_nodes(G, origin[1], origin[0])
+destination_node = ox.nearest_nodes(G, destination[1], destination[0])
+
+# Afficher les coordonnées de l'origine et de la destination
+print("Origine:", origin)
+print("Destination:", destination)
+
+# Calculer l'itinéraire aller et retour
+route = ox.shortest_path(G, origin_node, destination_node)
+route_back = ox.shortest_path(G, destination_node, origin_node)
+
+# Fonction pour convertir un itinéraire (liste de nœuds) en liste de coordonnées géographiques
+def route_to_coords(G, route):
+    route_coords = []
+    for node in route:
+        point = (G.nodes[node]['y'], G.nodes[node]['x'])  # latitude, longitude
+        route_coords.append(point)
+    return route_coords
+
+# Obtenir les coordonnées pour l'itinéraire aller et retour
+route_coords = route_to_coords(G, route)
+route_back_coords = route_to_coords(G, route_back)
+
+# Créer la carte centrée sur l'origine
+m = folium.Map(location=[origin[0], origin[1]], zoom_start=13)
+
+# Ajouter l'itinéraire aller (en rouge) à la carte
+folium.PolyLine(locations=route_coords, color='red', weight=5, opacity=0.75).add_to(m)
+
+# Ajouter l'itinéraire retour (en bleu) à la carte
+folium.PolyLine(locations=route_back_coords, color='blue', weight=5, opacity=0.75).add_to(m)
+
+# Ajouter des marqueurs pour l'origine et la destination
+folium.Marker(location=[origin[0], origin[1]], popup="Origine").add_to(m)
+folium.Marker(location=[destination[0], destination[1]], popup="Destination").add_to(m)
+
+# Sauvegarder la carte dans un fichier HTML
+m.save("./Cycle3/visualisation/carte_montpellier_trajet.html")
+
+# Afficher un message pour indiquer que la carte est prête
+print("La carte a été sauvegardée sous 'carte_montpellier.html'.")
+
+
---
+++ src/gestion_donnee.py
@@ -0,0 +1,36 @@
+import os
+import pandas as pd
+import pooch
+import zipfile
+
+class GestionnaireDonnees:
+    """
+    Une classe pour gérer le téléchargement, la lecture et le traitement des jeux de données.
+    """
+    def __init__(self, repertoire_telechargement="./data"):
+        self.repertoire_telechargement = repertoire_telechargement
+        os.makedirs(self.repertoire_telechargement, exist_ok=True)
+
+    def telecharger_fichier(self, url, nom_fichier):
+        chemin = pooch.retrieve(
+            url=url,
+            known_hash=None,
+            fname=nom_fichier,
+            path=self.repertoire_telechargement
+        )
+        return chemin
+
+    def charger_csv(self, chemin_fichier, supprimer_na=True):
+        dataframe = pd.read_csv(chemin_fichier)
+        if supprimer_na:
+            dataframe = dataframe.dropna()
+        return dataframe
+
+    def extraire_zip(self, chemin_zip, dossier_extraction=None):
+        if dossier_extraction is None:
+            dossier_extraction = os.path.join(self.repertoire_telechargement, "extracted")
+        os.makedirs(dossier_extraction, exist_ok=True)
+        with zipfile.ZipFile(chemin_zip, 'r') as zip_ref:
+            zip_ref.extractall(dossier_extraction)
+        os.remove(chemin_zip)
+        return dossier_extraction
